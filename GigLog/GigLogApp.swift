791a8e7058f097ecdc4aba7d0e53c9f2b9c24bf8
asheshp23 <asheshp23@gmail.com>
2026-02-20 19:16:42 -0500

    Initial Commit

---
+++ GigLog/GigLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  GigLogApp.swift
+//  GigLog
+//
+//  Created by Ashesh Patel on 2026-02-20.
+//
+
+import SwiftUI
+
+@main
+struct GigLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
